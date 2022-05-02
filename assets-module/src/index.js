94b431b38e34b29e1d023d5559ee5598cc207f0a
FeifanW <qihang791@163.com>
2022-05-02 23:11:25 +0800

    练习source资源

--- assets-module/src/index.js
+++ assets-module/src/index.js
@@ -1,6 +1,7 @@
 import helloWorld from './hello-word'
 import imgsrc from './assets/abc.png'
 import logoSvg from './assets/logo.svg'
+import exampleTxt from './assets/example.txt'
 
 
 helloWorld()
@@ -13,3 +14,8 @@ const img2 = document.createElement('img')
 img2.style.cssText = 'width: 600px; height: 200px'
 img2.src = logoSvg
 document.body.appendChild(img2)
+
+const block = document.createElement('div')
+block.style.cssText = 'width: 200px; height: 200px; background: aliceblue'
+block.textContent = exampleTxt
+document.body.appendChild(block)
